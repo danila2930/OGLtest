7e0df5c5be266d0180398024b1c5c2222601460b
danila goriachko <cohachka.ru@gmail.com>
2024-03-29 22:40:08 +0200

    firstTry

--- OGLtest/main.cpp
+++ OGLtest/main.cpp
@@ -14,7 +14,7 @@ const double PI = 3.14159265358979323846;
 struct Coord {
     float x;
     float y;
-    float rot;
+    float z;
 };
 
 
@@ -27,59 +27,13 @@ int drawLine(GLFWwindow* window, int StartX, int StartY, int EndX, int EndY) {
 
     float x2 = (EndX / (float)windowWidth) * 2.0f - 1.0f;
     float y2 = (EndY / (float)windowHeight) * 2.0f - 1.0f;
-    glBegin(GL_LINES);
-    glVertex2f(x, y);
-    glVertex2f(x2, y2);
-    glEnd();
+
 
 
 
     return 0;
 }
 
-int calculate(GLFWwindow* window, std::vector<Coord>& cords, int StartX, int StartY, float rotate, int curentnum) {
-
-    int windowWidth = 1000, windowHeight = 1000;
-    float x = (StartX / (float)windowWidth) * 2.0f - 1.0f;
-    float y = (StartY / (float)windowHeight) * 2.0f - 1.0f;
-
-    float EndX = 0;
-    float EndY = 0;
-    float size = 5;
-    float radians = rotate * PI / 180.0;
-
-    if (curentnum % 2 == 0) {
-        curentnum = curentnum / 2;
-
-        rotate -= 11;
-        EndX = StartX + size * cos(radians);
-        EndY = StartY + size * sin(radians);
-
-
-    }
-
-    else
-    {
-        curentnum = curentnum * 3 + 1;
-
-        rotate += 18;
-
-        EndX = StartX + size * cos(radians);
-        EndY = StartY + size * sin(radians);
-
-    }
-
-    cords.push_back({ EndX, EndY, rotate });
-
-
-    /*if (curentnum != 2) {
-        calculate(window, cords, EndX, EndY, rotate, curentnum, step);
-    }*/
-
-    return curentnum;
-
-
-}
 
 
 
@@ -107,27 +61,8 @@ int main(void)
     std::vector<Coord> cords;
 
 
-
-    for (int i = 0; i < 100000; i++)
-    {
-        cords.push_back({ 500, 200 , 90 });
-        int curnum = rand() % 10000000;
-        while (true)
-        {
-            Coord lastCoord = cords.back();
-            float lastX = lastCoord.x;
-            float lastY = lastCoord.y;
-            float lastrot = lastCoord.rot;
-            if (curnum < 4) {
-                break;
-            }
-            else
-            {
-                curnum = calculate(window, cords, lastX, lastY, lastrot, curnum);
-            }
-        }
-
-    }
+    cords.push_back({ 500, 200 , 90 });
+    
 
 
     /* Loop until the user closes the window */
@@ -138,15 +73,14 @@ int main(void)
 
 
 
-        for (size_t i = 0; i < cords.size() - 1; ++i) { // Итерируем до предпоследнего элемента
-            Coord currentCoord = cords[i]; // Текущая пара координат
-            Coord nextCoord = cords[i + 1]; // Следующая пара координат
+      /*for (size_t i = 0; i < cords.size() - 1; ++i) { 
+            Coord currentCoord = cords[i];
+            Coord nextCoord = cords[i + 1];
             if (nextCoord.x != 500)
             {
                 drawLine(window, currentCoord.x, currentCoord.y, nextCoord.x, nextCoord.y);
             }
-            // Ваш код здесь: работа с currentCoord и nextCoord
-        }
+        }*/
 
 
 
